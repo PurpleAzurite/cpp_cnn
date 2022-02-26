9d0fe26140ce5db6f0a45cc893e3ca182a2ed9d6
PurpleAzurite <misagh.asgari@gmail.com>
2022-02-26 17:58:03 +0300

    Integrate Network control into NetworkLayer

--- src/Layers/NetworkLayer.cpp
+++ src/Layers/NetworkLayer.cpp
@@ -1,17 +1,23 @@
+// clang-format off
 #include "NetworkLayer.h"
 #include <imgui.h>
 #include <iostream>
+#include <sstream>
+#include <string>
+// clang-format on
 
 namespace Engine {
 
-static void showVectorVals(string label, vector<double> &v)
+static std::string showVectorVals(std::string label, std::vector<double>& v)
 {
-    cout << label << " ";
-    for (unsigned i = 0; i < v.size(); ++i) {
-        cout << v[i] << " ";
-    }
+    std::stringstream ss;
+    ss << label << " ";
+    for (unsigned i = 0; i < v.size(); ++i)
+        ss << v[i] << " ";
 
-    cout << endl;
+    ss << '\n';
+
+    return ss.str();
 }
 
 NetworkLayer::NetworkLayer()
@@ -19,35 +25,6 @@ NetworkLayer::NetworkLayer()
     , m_td("data.dat")
     , m_net(m_td.topology_dont_call_unless_you_know_what_your_doing())
 {
-    vector<double> inputVals, targetVals, resultVals;
-    int trainingPass = 0;
-
-    while (!m_td.isEof())
-    {
-        ++trainingPass;
-        cout << endl << "Pass " << trainingPass;
-
-        // Get new input data and feed it forward:
-        if (m_td.getNextInputs(inputVals) != m_net.topology()[0])
-            break;
-
-        showVectorVals(": Inputs:", inputVals);
-        m_net.forward(inputVals);
-
-        // Collect the net's actual output results:
-        resultVals = m_net.results();
-        showVectorVals("Outputs:", resultVals);
-
-        // Train the net what the outputs should have been:
-        m_td.getTargetOutputs(targetVals);
-        showVectorVals("Targets:", targetVals);
-        assert(targetVals.size() == m_net.topology().back());
-
-        m_net.backward(targetVals);
-
-        // Report how well the training is working, average over recent samples:
-        cout << "Net recent average error: " << m_net.m_recentAvgError << endl;
-    }
 }
 
 void NetworkLayer::onAttach() {}
@@ -55,21 +32,52 @@ void NetworkLayer::onAttach() {}
 void NetworkLayer::onUpdate(double frameTime)
 {
     (void)frameTime;
+    std::vector<double> inputVals, targetVals, resultVals;
+    int trainingPass = 0;
+    static std::vector<std::string> text;
+
+    if (ImGui::Begin("Network"))
     {
-        // if (ImGui::Begin("Network"))
-        // {
-        //     ImGui::Text("No. shells %d", static_cast<int>(m_net.m_shells.size()));
-        //     ImGui::Separator();
+        if (ImGui::Button("Train"))
+        {
+            while (!m_td.isEof())
+            {
+                ++trainingPass;
+                text.emplace_back(std::string("Pass: " + std::to_string(trainingPass)));
 
-        //     for (unsigned int i = 0; i < m_net.m_shells.size(); ++i)
-        //     {
-        //         ImGui::Text("Shell %d\n\t%d nodes", i,
-        //                     static_cast<int>(m_net.m_shells.at(i).size()));
-        //     }
+                // Get new input data and feed it forward:
+                if (m_td.getNextInputs(inputVals) != m_net.topology()[0])
+                    break;
 
-        //     ImGui::Separator();
-        //     ImGui::End();
-        // }
+                text.emplace_back(showVectorVals("Inputs:", inputVals));
+                m_net.forward(inputVals);
+
+                // Collect the net's actual output results:
+                resultVals = m_net.results();
+                text.emplace_back(showVectorVals("Outputs:", resultVals));
+
+                // Train the net what the outputs should have been:
+                m_td.getTargetOutputs(targetVals);
+                text.emplace_back(showVectorVals("Targets:", targetVals));
+                assert(targetVals.size() == m_net.topology().back());
+
+                m_net.backward(targetVals);
+
+                // Report how well the training is working, average over recent samples:
+                text.emplace_back(std::string("Net recenet avg. error: " + std::to_string(m_net.m_recentAvgError)));
+            }
+        }
+
+        int count = 0;
+        for (const auto& i : text)
+        {
+            ++count;
+            ImGui::Text("%s", i.c_str());
+            if (count % 5 == 0)
+                ImGui::Separator();
+        }
+
+        ImGui::End();
     }
 }
 
